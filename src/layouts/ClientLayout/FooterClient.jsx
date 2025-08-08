26e0798a70d40dc1ded6a7d8036b34c1dfb78211
Phuong Anh <phamphuonganh5112002@gmail.com>
2025-08-08 17:29:54 +0700

    update UI HomePage

--- src/layouts/ClientLayout/FooterClient.jsx
+++ src/layouts/ClientLayout/FooterClient.jsx
@@ -1,7 +1,40 @@
+import { Button, Col, Row } from "antd";
+import TextArea from "antd/es/input/TextArea";
 import React from "react";
 
 const FooterClient = () => {
-  return <div>FooterClient</div>;
+  return (
+    <>
+      <Row>
+        <Col span={8}>
+          <div style={{ fontWeight: "bolder" }}>PERFECT COMPANION VIETNAM</div>
+          <div>Head Office:</div>
+          <div>
+            3rd Floor, No. 92 Yen The Street, Tan Son Hoa Ward, Ho Chi Minh City
+          </div>
+          <div>infor@perfectcompanion.com.vn</div>
+          <div>Tel: 028.3844.7470</div>
+        </Col>
+        <Col span={8}>
+          <div style={{ fontWeight: "bolder" }}>Policies & Regulations</div>
+          <div>Payment Methods</div>
+          <div>Privacy Policy</div>
+          <div>Return & Exchange Policy</div>
+        </Col>
+        <Col span={8}>
+          <div style={{ fontWeight: "bolder" }}>
+            Sign up to get the latest updates and exclusive offers from PERFECT
+            COMPANION VIETNAM.
+          </div>
+          <TextArea
+            placeholder="Email"
+            autoSize
+          />
+          <Button type="primary" style={{background: "#ffa201", fontWeight: "bolder"}}>Send Now</Button>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default FooterClient;
--- src/pages/client/HomePage.jsx
+++ src/pages/client/HomePage.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import ProductList from "./ProductList";
 import bannerPCV from "@assets/images/banner-pcv.jpg";
+import bannerDog from "@assets/images/banner-website-10-1.png.webp";
+import bannerCat from "@assets/images/banner-website-11.png.webp";
+import { Button, Card, Col, Row, Typography } from "antd";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+
+const cardStyle = {
+  height: "calc(100vh - 150px)",
+};
 
 const HomePage = () => {
   return (
@@ -12,7 +20,87 @@ const HomePage = () => {
       >
         <img src={bannerPCV} alt="Banner PCV" width={"100%"} />
       </div>
-      <ProductList />
+      <Card
+        hoverable
+        style={cardStyle}
+        styles={{
+          body: { padding: 0, overflow: "hidden", background: "#d3daff" },
+        }}
+      >
+        <Row gutter={[24, 24]} style={{ margin: "30px" }}>
+          <Col
+            span={12}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <img
+              alt="avatar"
+              src={bannerDog}
+              style={{ objectFit: "contain" }}
+              width={400}
+              height={400}
+            />
+          </Col>
+          <Col span={12}>
+            <Typography.Title level={3}>DOG FOOD</Typography.Title>
+            <Typography.Paragraph>
+              Every dog deserves to be nourished with high-quality food. They
+              are loyal companions and a source of motivation that helps us
+              become better every day. Nutritious and delicious meals, along
+              with a loving home, provide the best foundation for a dog’s
+              overall development.
+            </Typography.Paragraph>
+            <Button type="primary" danger>
+              View All
+            </Button>
+          </Col>
+        </Row>
+      </Card>
+      <Card
+        hoverable
+        style={cardStyle}
+        styles={{
+          body: { padding: 0, overflow: "hidden", background: "#fbe32fff" },
+        }}
+      >
+        <Row gutter={[24, 24]} style={{ margin: "30px" }}>
+          <Col span={12}>
+            <Typography.Title level={3}>CAT FOOD</Typography.Title>
+            <Typography.Paragraph>
+              The “royal highness” or “little princess” meow-meow may be quiet,
+              but they silently bring positive energy and soothe the soul of
+              those who care for them. These fur babies love to be petted,
+              cuddled, and are passionate about flavorful, nutritious, and
+              delicious meals. As a wise pet parent, choose the right dishes
+              your cat loves so that every mealtime is exciting and never
+              boring.
+            </Typography.Paragraph>
+            <Button type="primary" danger>
+              View All
+            </Button>
+          </Col>
+          <Col
+            span={12}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <img
+              alt="avatar"
+              src={bannerCat}
+              style={{ objectFit: "contain" }}
+              width={400}
+              height={400}
+            />
+          </Col>
+        </Row>
+      </Card>
+      {/* <ProductList /> */}
     </>
   );
 };
